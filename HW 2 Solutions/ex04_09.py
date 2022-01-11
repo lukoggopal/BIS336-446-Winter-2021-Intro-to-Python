72c4900ecdaca66d6960ff0465066bc26831600c
Troy Adair <tra418@lehigh.edu>
2022-01-11 13:53:07 -0500

    Corrected Solution 4-09

--- HW 2 Solutions/ex04_09.py
+++ HW 2 Solutions/ex04_09.py
@@ -2,7 +2,7 @@
 def fahrenheit(celsius):
     return (9 / 5) * celsius + 32
 
-print(f'Fahrenheit{"Celsius":>10}')
+print('Celcius', '\t','Fahrenheit')
 
 for celsius in range(101):
     print(f'{celsius:10}{fahrenheit(celsius):10.1f}')
